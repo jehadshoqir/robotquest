8d2df2f4c0d0bae23e67c8cfc53fe9a51eb73d9d
jehadshoqir <jehadshoqir@gmail.com>
2018-11-30 20:12:01 +0100

    This is my code

--- ex2/src/robotquest-features.js
+++ ex2/src/robotquest-features.js
@@ -2,60 +2,44 @@ module.exports =
     {turn, move, updateBoard, checkIfFlagReached, cloneRobot};
 
 const SYMBOLS = {
-    robot: SetReverse('R'),
+    robot: setReverse('R'),
     tree: colorInGreen('T'),
     flag: colorInYellow('F'),
-    water: setBlueBg('w'),
+    water: setBlueBg('w')
 
 }
 module.exports.SYMBOLS = SYMBOLS;
 
 const trailIndicators = {
-    left: setBright('←'),
-    right: setBright('→'),
-    up: setBright('↑'),
-    down: setBright('↓')
+    left: setBright('?'),
+    right: setBright('?'),
+    up: setBright('?'),
+    down: setBright('?')
 };
 
-//Here we test
+//Here
 function turn(robot, step, turns) {
-    console.log(step);
-    console.log(robot.head);
-    turns += 1;
-    //
-    switch (step.trim()) {
-        case 'right':
-            switch (robot.head){
-                case 'up': robot.head='right';break;
-                case 'right': robot.head='down'; break;
-                case 'down': robot.head='left';break;
-                case 'left': robot.head='up'; break;
-            }
-            console.log('Im inside the case' + robot.head);
-            break;
-        case 'turn-left':
-            switch (robot.head) {
-                case 'left': robot.head='down'; break;
-                case 'down': robot.head='right'; break;
-                case 'right': robot.head='up'; break;
-                case 'up': robot.head='left'; break;
-            }
-            console.log('im inside the case left');
-            break;
+   nbOfTurns = turns+1;
+   if(robot.head = 'up' && step == 'right')
+       robot.head = 'right';
+   else
+       nbOfTurns = turns+1;
+       if(robot.head = 'left' && step == 'turn-left')
+       robot.head='down';
+
+return nbOfTurns;
 
 
-    }
-    console.log(robot.head);
-    return turns;
 }
 
 function move(robot, maxLineIndex, maxColumnIndex, nbOfMoves) {
+    robot.head = 'right';
     nbOfMoves += 1;
     return nbOfMoves;
 }
 
 function updateBoard(board, previousRobotState, currentRobotState) {
-    return updateBoard();
+    return;
 }
 
 function checkIfFlagReached(robot, board) {
@@ -74,24 +58,22 @@ function cloneRobot(robot) {
 
 
 // presentation utils
-/*
-function colorInGreen("green") {
-    return green;
+function colorInGreen(char) {
+    return `\x1b[32m${char}\x1b[0m`;
 }
 
-function colorInYellow("yellow") {
-    return yellow;
+function colorInYellow(char) {
+    return `\x1b[33m${char}\x1b[0m`;
 }
 
-function setBlueBg("blue") {
-    return blue;
+function setBlueBg(char) {
+    return `\x1b[44m${char}\x1b[0m`;
 }
 
-function setReverse("reverse") {
-    return reverse;
+function setReverse(char) {
+    return `\x1b[7m${char}\x1b[0m`;
 }
 
-function setBright("right") {
-    return right;
+function setBright(char) {
+    return `\x1b[1m${char}\x1b[0m`;
 }
-*/
--- ex2/test/robotquest.test.js
+++ ex2/test/robotquest.test.js
@@ -1,6 +1,7 @@
 const featuresToTest = require('../src/robotquest-features');
-// right direction
+
 test('robot should have its head turned to the right when head is up and direction is right', () => {
+
     let robot = {
         position: {
             line: 0,
@@ -9,55 +10,12 @@ test('robot should have its head turned to the right when head is up and directi
         head: 'up'
     };
 
-    let nbOfTurn = featuresToTest.turn(robot, 'right', 0);
-    expect(nbOfTurn).toBe(1);
+    let nbOfTurn = featuresToTest.turn(robot, 'right', 1);
+    expect(nbOfTurn).toBe(2);
     expect(robot.head).toBe('right');
 });
 
-test('robot should have its head turned to the down when head is right and direction is right', () => {
-    let robot = {
-        position: {
-            line: 0,
-            column: 0
-        },
-        head: 'right'
-    };
 
-    let nbOfTurn = featuresToTest.turn(robot, 'right', 0);
-    expect(nbOfTurn).toBe(1);
-    expect(robot.head).toBe('down');
-});
-
-test('robot should have its head turned to the left when head is down and direction is right', () => {
-    let robot = {
-        position: {
-            line: 0,
-            column: 0
-        },
-        head: 'down'
-    };
-
-    let nbOfTurn = featuresToTest.turn(robot, 'right', 0);
-    expect(nbOfTurn).toBe(1);
-    expect(robot.head).toBe('left');
-});
-
-
-test('robot should have its head turned to the up when head is left and direction is right', () => {
-    let robot = {
-        position: {
-            line: 0,
-            column: 0
-        },
-        head: 'left'
-    };
-
-    let nbOfTurn = featuresToTest.turn(robot, 'right', 3);
-    expect(nbOfTurn).toBe(4);
-    expect(robot.head).toBe('up');
-});
-
-//left direction
 test('robot should have its head down when head is left and direction is left', () => {
     let robot = {
         position: {
@@ -70,62 +28,20 @@ test('robot should have its head down when head is left and direction is left', 
     let nbOfTurn = featuresToTest.turn(robot, 'turn-left', 8);
     expect(nbOfTurn).toBe(9);
     expect(robot.head).toBe('down');
+
 });
-
-test('robot should have its head right when head is down and direction is left', () => {
-    let robot = {
-        position: {
-            line: 0,
-            column: 0
-        },
-        head: 'down'
-    };
-
-    let nbOfTurn = featuresToTest.turn(robot, 'turn-left', 8);
-    expect(nbOfTurn).toBe(9);
-    expect(robot.head).toBe('right');
-});
-
-test('robot should have its head up when head is right and direction is left', () => {
-    let robot = {
-        position: {
-            line: 0,
-            column: 0
-        },
-        head: 'right'
-    };
-
-    let nbOfTurn = featuresToTest.turn(robot, 'turn-left', 8);
-    expect(nbOfTurn).toBe(9);
-    expect(robot.head).toBe('up');
-});
-
-test('robot should have its head left when head is up and direction is left', () => {
-    let robot = {
-        position: {
-            line: 0,
-            column: 0
-        },
-        head: 'up'
-    };
-
-    let nbOfTurn = featuresToTest.turn(robot, 'turn-left', 8);
-    expect(nbOfTurn).toBe(9);
-    expect(robot.head).toBe('left');
-});
-
-
-
 // TODO: write some more tests on turn()
 
+
 test('robot should move 1 step up when head is up', () => {
+
     let maxLineIndex = 3; // 4 lines
     let maxColumnIndex = 3; // 4 columns
     let nbOfMoveAlreadyDone = 5;
     let robot = {
         position: {
-            line:0,
-            column: 0,
+            line: 2,
+            column: 2
         },
         head: 'up'
     };
@@ -134,35 +50,35 @@ test('robot should move 1 step up when head is up', () => {
     expect(nbOfMove).toBe(6);
     expect(robot.position.column).toBe(2);
     expect(robot.position.line).toBe(3);
+
 });
 
 // TODO: write some more tests on move()
 
+
 test('robot reaches the flag when its position meets `F` on the board', () => {
+
     let board = [
         ['.', 'F'] // only one line to this board
+
     ];
     let winningRobot = {
         position: {
             line: 0,
-            column:0,
+            column: 1
+        },
 
-},
         head: 'right'
     };
 
     let anotherRobot = {
         position: {
             line: 0,
-            column: 1
+            column: 0
         },
         head: 'right'
     };
-
-
     expect(featuresToTest.checkIfFlagReached(winningRobot, board)).toBe(true);
     expect(featuresToTest.checkIfFlagReached(anotherRobot, board)).toBe(false);
-
 });
-
 // TODO: write some more tests on checkIfFlagReached
